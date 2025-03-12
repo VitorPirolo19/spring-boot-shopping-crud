d3caa80ddef654a4f3765afa263a3a99bc7ba529
VitorPirolo19 <vpirolo10@gmail.com>
2025-03-11 23:27:20 -0300

    feat: user service layer

--- src/main/java/com/vitor/course/controller/UserController.java
+++ src/main/java/com/vitor/course/controller/UserController.java
@@ -1,18 +1,32 @@
 package com.vitor.course.controller;
 
 import com.vitor.course.entities.User;
+import com.vitor.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L,"Vitor","vitor@email","2422323","2323234");
-        return ResponseEntity.ok().body(u); 
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id ){
+        User user = service.findById(id);
+        return ResponseEntity.ok().body(user);
     }
 }
